c9253be0702de702cdad8f460cb3caf88071efb4
wobushirunning <wuhoudesun@126.com>
2019-01-12 01:57:31 +0800

    静态文件添加

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@ export default {
 </script>
 
 <style lang="less">
+@import "assets/fonts/fonts.css";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
